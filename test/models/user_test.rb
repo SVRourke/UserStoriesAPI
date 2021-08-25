f5695b5727059628da5e220e7944fa55a1e99b1d
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 12:56:56 -0400

    stubbing out basic tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Valid User" do
+    user = User.new(username: "SVRourke", email: "samuelvanrourke@gmail.com")
+    assert user.valid?
+  end
+  
+  test "invalid if username taken" do
+    validUser = User.create(username: "SVRourke", email: "samuelvanrourke@gmail.com")
+    invalidUser = User.new(username: "SVRourke", email: "s@gmail.com")
+    assert_not invalidUser.valid?
+  end
+  
+  test "invalid without name" do
+    user = User.new(email: "s@gmail.com")
+    assert_not user.valid?
+  end
+  
+  test "invalid without email" do
+    user = User.new(username: "s")
+    assert_not user.valid?
+  end
+
 end
