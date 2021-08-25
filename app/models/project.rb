2ac6d8de76030351c424e0897d4c3c47f0400ad9
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 15:59:55 -0400

    added dependent destroy to the relationship between projects and epics

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
     validates :name, {presence: true, uniqueness: {scope: :user, message: "project already exists"}}
     belongs_to :user
+
+    has_many :epics, dependent: :destroy
+
 end
