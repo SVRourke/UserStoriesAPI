763fd4392cb56486d3fc4dd8cece42b448f46751
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 14:57:16 -0400

    validates email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, {presence: true, uniqueness: true}
-    validates :email, presence: true
+    validates :email, {presence: true, uniqueness: true}
 
 
     has_many :projects
