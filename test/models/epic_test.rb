b6ee252242f14a962e5fc179f22e343399669aa7
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 16:01:57 -0400

    created basic tests for epic validations

--- test/models/epic_test.rb
+++ test/models/epic_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class EpicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(username: "SVR", email: "s@s.com")
+    @project = Project.create(name: "TEST PROJECT", user: @user, description: "slkdflskdm")
+    @validEpic = @project.epics.create(content: "this is a test Epic")
+    @noContent = @project.epics.new()
+    @noProject = Epic.new(content: "blahblahblahblah")
+    @duplicate = @project.epics.new(content: "this is a test Epic")
+  end
+  
+  def teardown
+    @user.destroy!
+    @project.destroy!
+    @validEpic.destroy!
+  end
+
+  test "valid epic" do
+    assert @validEpic.valid?
+  end
+  
+  test "invalid without content" do
+    assert_not @noContent.valid?    
+  end
+  
+  test "invalid without project" do
+    assert_not @noProject.valid?    
+  end
+  
+  test "invalid if epic is a duplicate within a given project" do
+    assert_not @duplicate.valid?
+  end
+
+
 end
