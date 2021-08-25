010325b29f45514fd2f301f89efb2d28ec3440a8
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 15:16:11 -0400

    basic tests worked out

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,31 @@
-require "test_helper"
+# require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(username: "SVRourke", email: "s@s.com")
+    @validProject = Project.create(user: @user, name: "First", description: "this is a test")
+    @noName = Project.new(user: @user, description: "this is a test")
+    @noUser = Project.new(name: "First", description: "this is a test")
+    @duplicate = Project.new(user: @user, name: "First", description: "this is a test")
+    
+  end
+  
+  def teardown
+  end
+
+  test "valid project" do
+    assert @validProject.valid?
+  end
+  
+  test "invalid without name" do
+    assert_not @noName.valid?
+  end
+  
+  test "invalid without user" do
+    assert_not @noUser.valid?
+  end
+  
+  test "invalid if the user already has a project with the same name" do
+    assert_not @duplicate.valid?
+  end
 end
