5969872ef54ca788e5074ddde5f7374e48e08b5e
SVRourke <samuelvanrourke@gmail.com>
2021-08-25 14:04:22 -0400

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_162749) do
+ActiveRecord::Schema.define(version: 2021_08_25_174626) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
