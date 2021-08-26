d5f5e6b83c8fb651079af484da0834674da0c56e
SVRourke <samuelvanrourke@gmail.com>
2021-08-26 09:49:59 -0400

    add dependent destroy to Epic->Stories and Epic->comments

--- app/models/epic.rb
+++ app/models/epic.rb
@@ -2,6 +2,6 @@ class Epic < ApplicationRecord
     validates :content, {presence: true, uniqueness: {scope: :project, message: "Project already contains that epic"}}
     
     belongs_to :project
-    has_many :stories
-    has_many :comments, as: :commentable
+    has_many :stories, dependent: :destroy
+    has_many :comments, dependent: :destroy, as: :commentable
 end
