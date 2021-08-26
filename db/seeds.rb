598242da0457a86c80c7d68e8d9cb9f6b7eebabd
SVRourke <samuelvanrourke@gmail.com>
2021-08-26 09:40:11 -0400

    added seeds for experimenting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "SVRourke", email: "s@s.com")
+project = user.projects.create(name: "Project 1", description: "this is a project")
+project.comments.create(content: "This is a comment")
+
+epic = project.epics.create(content: "This is an epic")
+epic.comments.create(content: "This is a comment")
+
+story = epic.stories.create(content: "this is a story")
+epic.stories.create(content: "this is another story")
+epic.stories.create(content: "this is yet another story")
+
+story.comments.create(content: "This is a comment")
+story.comments.create(content: "This is another comment")
+story.comments.create(content: "This is yet another comment")
