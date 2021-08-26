098bca7a5adc9aba4c4d8e1beb1baee5916b2ddc
SVRourke <samuelvanrourke@gmail.com>
2021-08-26 09:40:29 -0400

    made complete default to false

--- db/migrate/20210826115708_create_stories.rb
+++ db/migrate/20210826115708_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration[6.1]
   def change
     create_table :stories do |t|
       t.string :content
-      t.boolean :complete
+      t.boolean :complete, default: false
       t.belongs_to :epic
 
       t.timestamps
