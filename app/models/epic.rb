053560ae9f2ce21368d304f510d8e11bf6f68b22
SVRourke <samuelvanrourke@gmail.com>
2021-08-26 08:03:16 -0400

    add relationship with stories

--- app/models/epic.rb
+++ app/models/epic.rb
@@ -2,5 +2,5 @@ class Epic < ApplicationRecord
     validates :content, {presence: true, uniqueness: {scope: :project, message: "Project already contains that epic"}}
     
     belongs_to :project
-
+    has_many :stories
 end
