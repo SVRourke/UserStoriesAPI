8669c9738aa1df3022aac8884a1f9bdbeecdd145
SVRourke <samuelvanrourke@gmail.com>
2021-08-27 11:16:14 -0400

    comments for needed actions basic index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        render json: {
+            users: User.all
+        }
+    end
+    # create
+    # read
+    # update
+    # destroy
 end
+#
