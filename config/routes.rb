4e4d3c9ab7ea8e4fd8cbeb0c6cc533b98294889e
SVRourke <samuelvanrourke@gmail.com>
2021-08-27 11:17:08 -0400

    comment out resources, fleshed out user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :stories
-  resources :epics
-  resources :projects
-  resources :users
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  # resources :comments
+  # resources :stories
+  # resources :epics
+  # resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
